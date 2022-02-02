4fd8cf872dcd9e8a996b7d6d51deb508a900632b
Neguin0 <65922949+Neguin0@users.noreply.github.com>
2022-02-02 05:50:14 -0300

    init

---
+++ index.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const app = new Discord.Client();
+
+app.on('ready', () => {
+      console.log('Estou Online!!');
+});
+
+app.on('message', (msg) => {
+	if (msg.content === 'join' && msg.member.voice.channel && !msg.author.bot){
+		msg.member.voice.channel.join().then(connection => {
+			const dispatcher = connection.play('entrou.mp3');
+			dispatcher.on('finish', () => { msg.member.voice.channel.leave() });
+		});
+	}
+});
+app.login('OTM4MzQxNTE4Nzk0NzAyODk4.Yfo4ow.qUc5jS8oKYeEKFBT6GAUA1hCuhg');
