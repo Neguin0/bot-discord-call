05d69811233369676a487d1d3fe387af381caa7a
Neguin0 <65922949+Neguin0@users.noreply.github.com>
2022-02-02 06:39:31 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -6,19 +6,21 @@ app.on('ready', () => {
 });
 
 app.on('message', (msg) => {
-	if (msg.content === 'test' && msg.member.voice.channel && !msg.author.bot){
-		//console.log(msg.member.voice.channel);
+	/*if (msg.content === 'test' && msg.member.voice.channel && !msg.author.bot){
+		console.log("Mensagem >>", msg.member);
 		msg.member.voice.channel.join().then(connection => {
 			const dispatcher = connection.play('entrou.mp3');
 			dispatcher.on('finish', () => { msg.member.voice.channel.leave() });
 		});
-	}
+	}*/
 });
 
 app.on('voiceStateUpdate', (oldState, newState)=>{
-	newState.join().then(connection => {
-		const dispatcher = connection.play('entrou.mp3');
-		dispatcher.on('finish', () => { newState.leave() });
-	});
+	if(newState.channel){
+		newState.channel.join().then(connection => {
+			const dispatcher = connection.play('entrou.mp3');
+			dispatcher.on('finish', () => { newState.leave() });
+		});
+	}
 })
 app.login('OTM4MzQxNTE4Nzk0NzAyODk4.Yfo4ow.qUc5jS8oKYeEKFBT6GAUA1hCuhg');
