4956c8b1a8a84ed731127c22dea86b1533a1fb38
Neguin0 <65922949+Neguin0@users.noreply.github.com>
2022-02-17 00:44:49 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,32 +1,23 @@
 const Discord = require('discord.js');
+const googleTTS = require('google-tts-api');
 const app = new Discord.Client();
-const express = require('express')();
-const port = process.env.PORT || 3000
-express.get('/', (req, res) =>{
-	res.send("Bot dos cria!");
-});
-express.listen(port, ()=>{
-	console.log('Server Online!');
-});
 
 app.on('ready', () => {
-      console.log('Estou Online!!');
+	console.log('Estou Online!!');
 });
 
-app.on('message', (msg) => {
-	/*if (msg.content === 'test' && msg.member.voice.channel && !msg.author.bot){
-		console.log("Mensagem >>", msg.member);
-		msg.member.voice.channel.join().then(connection => {
-			const dispatcher = connection.play('entrou.mp3');
-			dispatcher.on('finish', () => { msg.member.voice.channel.leave() });
-		});
-	}*/
-});
-
-app.on('voiceStateUpdate', (oldState, newState)=>{
-	if(newState.channel){
-		newState.channel.join().then(connection => {
-			const dispatcher = connection.play('entrou.mp3');
+app.on('voiceStateUpdate', async(oldState, newState) => {
+	if((newState && newState.user && newState.user.bot) || (oldState && oldState.user && oldState.user.bot)) return;
+	if((newState.mute && !oldState.mute) || (!newState.mute && oldState.mute)) return;
+	if((newState.streaming && !oldState.streaming) || (!newState.streaming && oldState.streaming)) return;
+	if (newState.channel && !newState.member.user.bot) {
+		newState.channel.join().then(async (connection) => {
+			const url = await googleTTS.getAudioUrl(`${newState.member.nickname || newState.member.user.username} entrou na call para mamar a tropa!`, {
+				lang: 'pt-BR',
+				slow: false,
+				host: 'https://translate.google.com',
+			});
+			const dispatcher = await connection.play(url);
 			dispatcher.on('finish', () => { newState.channel.leave() });
 		});
 	}
